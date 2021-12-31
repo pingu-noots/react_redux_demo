23b89b301ffc5d539ed50797d22345bd807947c0
pingue-noots <high.bridge031900@gmail.com>
2021-12-31 15:11:45 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 function App() {
+  const profile = [
+    {name: "taro", age: 10},
+    {name: 'takashi', age: '40'},
+    {name: 'noName'}
+  ]
   return (
     <div className="App">
-	    <h1>Hello World!!!</h1>
+      {
+        profile.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   );
+}
+
+const User = (props) => {
+  return <div>Hi!, I am {props.name}! and I am {props.age}</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
